f10708bb4bd14aeddfd7f25edf574aa97110fb3e
Wiktoria Drabińska <wiktoria.drabinska@gmail.com>
2026-03-16 17:45:41 +0100

    added extracting sound

--- dataset/process_video.py
+++ dataset/process_video.py
@@ -1,5 +1,6 @@
 from moviepy import VideoFileClip
 import numpy as np
+import os
 
 def process_video(input_path: str, output_path: str):
 
@@ -7,12 +8,28 @@ def process_video(input_path: str, output_path: str):
 
     name = input_path.split("/")[-1].split(".")[-2]
 
+    if not os.path.exists(output_path + "images"):
+
+        os.makedirs(output_path + "images")
+
+    if not os.path.exists(output_path + "sound"):
+
+        os.makedirs(output_path + "sound")
+
     for clip_start in range(0, int(video.duration) - 10, 10):
 
         clip = video.subclipped(clip_start, clip_start + 10)
 
         frame_start = clip.get_frame(0)
 
-        with open(output_path + "images/" + name + str(clip_start // 10) + ".npy", "xb") as image_file:
+        clip_name = name + str(clip_start // 10)
+
+        with open(output_path + "images/" + clip_name + ".npy", "xb") as image_file:
 
             np.save(image_file, frame_start)
+
+        audio = clip.audio
+
+        with open(output_path + "sound/" + clip_name + ".npy", "xb") as image_file:
+
+            np.save(image_file, audio.to_soundarray())
